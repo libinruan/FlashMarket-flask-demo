3ba2d2f094d9ab9a010ea78be43e6631c5526161
Libin Ruan <lipin.juan02@gmail.com>
2024-02-18 16:35:33 -0500

    finished part 10 advanced validation

--- market/__init__.py
+++ market/__init__.py
@@ -1,9 +1,9 @@
-from flask import Flask  #, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '03defa46bf740bdbe81d7601'
 db = SQLAlchemy(app)  # to pass our application instance to SQLAlchemy
-
 from market import routes
